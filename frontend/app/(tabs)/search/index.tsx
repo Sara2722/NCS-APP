8f9c92176e67ef17631e60c858615e2727c29d1d
Phoebe-Hope <phoebe.hope345@gmail.com>
2026-02-06 19:33:09 +0000

    Set up search page with search bar and button

---
+++ frontend/app/(tabs)/search/index.tsx
@@ -0,0 +1,79 @@
+import { StyleSheet, 
+  Text, 
+  View, 
+  TextInput, 
+  Pressable, 
+  Keyboard,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useState } from "react";
+import TopBar from '@/components/TopBar'
+import { useRouter } from "expo-router";
+
+
+
+
+export default function search() {
+  const [query, setQuery] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = () => {
+    const trimmed = query.trim();
+    if (!trimmed) return;
+
+    Keyboard.dismiss();
+
+    router.push({
+      pathname: "/search/results",
+      params: { q: trimmed },
+    });
+  };
+  
+  return (
+    <View style={styles.container}>
+      <SafeAreaView edges={['top']} style={styles.topSafeArea}/>
+      <TopBar />
+      <View 
+      style={styles.content} 
+      className="px-4 pt-4 pb-4 flex-1">
+        <Text className="text-2xl font-semibold mb-4 text-center text-black">Search</Text>
+      
+      <View className="flex-row items-center gap-2">
+        <TextInput
+          className="flex-1 border border-gray-300 rounded-xl px-4 py-2 text-base"
+          placeholder="Search..."
+          value={query}
+          onChangeText={setQuery}
+          returnKeyType="search"
+          onSubmitEditing={handleSubmit}/>
+      
+      <Pressable
+        onPress={handleSubmit}
+        className="bg-black px-4 py-2 rounded-xl"
+        >
+          <Text className="text-white font-medium">Go</Text>
+        </Pressable>
+      </View>
+      
+      
+      </View>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF"
+  },
+  topSafeArea: {
+    backgroundColor: '#FFDD00',
+  },
+  content: {
+    flex: 1,
+  },
+  scrollContent: {
+    paddingBottom: 20,
+  },
+})
