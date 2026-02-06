3bbb2c79e6b4e2df95cbc9487bbfb46fd83150cd
Phoebe-Hope <phoebe.hope345@gmail.com>
2026-02-06 20:17:08 +0000

    Added Item type and mock data

--- frontend/app/(tabs)/search/results.tsx
+++ frontend/app/(tabs)/search/results.tsx
@@ -1,21 +1,44 @@
 import { StyleSheet, 
   Text, 
   View, 
-  TextInput, 
-  Pressable, 
-  Keyboard,
+  FlatList, 
+  Pressable,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
-import React, { useState } from "react";
+import React, { useMemo } from "react";
 import TopBar from '@/components/TopBar'
-import { useRouter } from "expo-router";
+import { useRouter, useLocalSearchParams } from "expo-router";
 
+type Item = {
+    id: string;
+    title: string;
+    description: string;
+};
 
+{/* MOCK DATA */}
 
+const MOCK_ITEMS: Item[] = [
+    {
+        id: "1",
+        title: "Example 1",
+        description: "This is the first example."
+    },
+    {
+        id: "2",
+        title: "Example 2",
+        description: "This is the second example."
+    },
+    {
+        id: "3",
+        title: "Example 3",
+        description: "This is the third example."
+    },
+]
 
+{/*}
 export default function search() {
-  const [query, setQuery] = useState("");
   const router = useRouter();
+  const params = useLocalSearchParams
 
   const handleSubmit = () => {
     const trimmed = query.trim();
@@ -36,13 +59,13 @@ export default function search() {
       <View 
       style={styles.content} 
       className="px-4 pt-4 pb-4 flex-1">
-        <Text className="text-2xl font-semibold mb-4 text-center text-black">Search</Text>
+        <Text className="text-2xl font-semibold mb-4 text-center text-black">Search Results</Text>
       </View>
     </View>
   );
 }
 
-
+*/}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
